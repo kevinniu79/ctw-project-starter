618061d661ee0c18091735702c27d66cc008b3a9
Sirat Mokha <mokha1671@gmail.com>
2022-06-22 09:57:13 -0500

    Added stepper dial

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,10 +1,19 @@
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
 import Link from '@mui/material/Link';
+
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import PrintIcon from '@mui/icons-material/Print';
+import ShareIcon from '@mui/icons-material/Share';
 import ROUTES from '../../constants/routes';
 
 export default function Home() {
@@ -23,6 +32,13 @@ export default function Home() {
   const onStudentListClick = () => {
     navigate(ROUTES.STUDENTS);
   };
+
+  const actions = [
+    { icon: <FileCopyIcon />, name: 'Copy' },
+    { icon: <SaveIcon />, name: 'Save' },
+    { icon: <PrintIcon />, name: 'Print' },
+    { icon: <ShareIcon />, name: 'Share' },
+  ];
 
   return (
     <Container maxWidth="sm">
@@ -86,6 +102,24 @@ export default function Home() {
           </Button>
         </Stack>
       </Box>
+
+    
+      <Box sx={{ height: 320, transform: 'translateZ(0px)', flexGrow: 1 }}>
+      <SpeedDial
+        ariaLabel="SpeedDial basic example"
+        sx={{ position: 'absolute', bottom: 16, right: 16 }}
+        icon={<SpeedDialIcon />}
+      >
+        {actions.map((action) => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+          />
+        ))}
+      </SpeedDial>
+    </Box>
+
     </Container>
   );
 }
