8d77a46ce76c57f800646da024e509aa11c2f3c2
Sirat Mokha <mokha1671@gmail.com>
2022-06-22 10:11:13 -0500

    Added modal button

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 
@@ -6,7 +7,7 @@ import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
 import Link from '@mui/material/Link';
-
+import Modal from '@mui/material/Modal';
 import SpeedDial from '@mui/material/SpeedDial';
 import SpeedDialIcon from '@mui/material/SpeedDialIcon';
 import SpeedDialAction from '@mui/material/SpeedDialAction';
@@ -39,6 +40,23 @@ export default function Home() {
     { icon: <PrintIcon />, name: 'Print' },
     { icon: <ShareIcon />, name: 'Share' },
   ];
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+  
+  
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
   return (
     <Container maxWidth="sm">
@@ -119,6 +137,25 @@ export default function Home() {
         ))}
       </SpeedDial>
     </Box>
+
+    <div>
+      <Button variant="contained" onClick={handleOpen}>Open modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Test modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            This is an example of a modal 
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
 
     </Container>
   );
