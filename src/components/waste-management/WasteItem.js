5507dbf9adb9f065623f72869b5daf96ef536d98
Holly R <hraetz14@gmail.com>
2022-06-22 14:09:58 -0500

    added WasteItem.js and set up states

---
+++ src/components/waste-management/WasteItem.js
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import React, {useState} from 'react';
+
+export default function WasteItem() {
+    const [owner, setOwner] =               useState('');
+    const [name, setname] =                 useState('');
+    const [price, setPrice] =               useState('');
+    const [city, setCity] =                 useState('');
+    const [state, setState] =               useState('');
+    const [postalCode, setPostalCode] =     useState('');
+    const [dateAccepted, setDateAccepted] = useState('');
+    const [dateReturned, setDateReturned] = useState('');
+
+    return(
+        <div>fill</div>
+    );
+}
