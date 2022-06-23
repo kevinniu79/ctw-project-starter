cace5d83ecf3231dd0ff461fdf3626d1c191ef10
Noah Ren <nren2025@gmail.com>
2022-06-23 11:25:59 -0500

    Working on Return

--- src/components/waste-management/WasteItem.js
+++ src/components/waste-management/WasteItem.js
@@ -1,8 +1,18 @@
 /* eslint-disable prettier/prettier */
-import React, {useState} from 'react';
-import WasteList from './Waste-List';
-import AddWasteItem from './waste-management-form/add-waste-item';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { useState, useEffect } from 'react';
+import AddStudent from './student-forms/add-student';
+import StudentsList from './students-list';
+import UpdateStudent from './student-forms/update-student';
+import {
+  getStudents,
+  addStudent,
+  updateStudent,
+} from '../../services/services';
 import UpdateWasteManagement from './waste-management-form/update-waste-management';
+import WasteList from './Waste-List';
 
 export default function WasteItem() {
     const [owner, setOwner] =               useState('');
@@ -14,11 +24,49 @@ export default function WasteItem() {
     const [dateAccepted, setDateAccepted] = useState('');
     const [dateReturned, setDateReturned] = useState('');
 
-    return(
-        <div>
-            <AddWasteItem/>
-            <UpdateWasteManagement/>
-            <WasteList/>
-        </div>
-    );
+    return (
+        <Container maxWidth="sm">
+          {UpdateWasteManagement && (
+            <UpdateWasteItem
+              WasteItem={wasteItemToUpdate}
+              onSubmit={updateItemHandler}
+              handleClose={updateModalChange}
+            />
+          )}
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Typography component="h1" variant="h5">
+              Students
+            </Typography>
+            <AddWasteItem
+                owner={setOwner}
+                name={setName}
+                price={setPrice}
+                city={setCity}
+                state={setState}
+                postalCode={setPostalCode}
+                dateAccepted={setDateAccepted}
+                dateReturned={setDateReturned}
+            />
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <WasteList
+              wasteItem={Waste}
+              archiveItemListandler={archiveItemListHandler}
+              updateItemListHandler={updatWasteItemModalChange}
+            />
+          </Box>
+        </Container>
+      );
 }
