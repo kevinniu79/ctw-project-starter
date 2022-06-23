704bee00560825a426218e6f7a6dbcc36793aaae
Kevin Niu <kevin.h.niu@gmail.com>
2022-06-23 12:13:15 -0500

    adding waste items to services

--- src/components/waste-management/WasteItem.js
+++ src/components/waste-management/WasteItem.js
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 import WasteList from './Waste-List';
 import AddWasteItem from './waste-management-form/add-waste-item';
 import UpdateWasteManagement from './waste-management-form/update-waste-management';
+import { getWasteList, addWaste, updateWaste } from '../../services/services';
 
 export default function WasteItem() {
     const [owner, setOwner] =               useState('');
