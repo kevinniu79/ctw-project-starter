751144c59fc1332554fb8fc32e0dd9b5d4ccb4c1
Kevin Niu <kevin.h.niu@gmail.com>
2022-06-23 10:20:09 -0500

    fixing WasteItem variable name

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -2,8 +2,8 @@ const ROUTES = {
   HOME: '/',
   SIGN_UP: '/sign-up',
   STUDENTS: '/students',
-  SIGN_UP_SUCCESS: '/sign-up-succes',
-  WASTE_MANAGEMENT: '/waste-management',
+  SIGN_UP_SUCCESS: '/sign-up-success',
+  WASTE_MANAGEMENT: '/WasteItem',
 };
 
 export default ROUTES;
