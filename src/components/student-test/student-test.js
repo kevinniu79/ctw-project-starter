27c2780f791a7e362ed5b0e80b1a4a4061e7fc34
Aaditya Tiwari (BCHS) <24aaditf@gmail.com>
2022-07-11 11:32:17 -0500

    Added student info component directly into student test

--- src/components/student-test/student-test.js
+++ src/components/student-test/student-test.js
@@ -1,7 +1,10 @@
 import Grid from '@mui/material/Grid';
+import { experimentalStyled as styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 import StudentModal from './studentModal';
 
-export default function Student() {
+export default function Student(props) {
   const deleteHandler = () => {
     console.log('this is when it should delete');
   };
@@ -11,6 +14,15 @@ export default function Student() {
   const reactivateHandler = () => {
     console.log('this is when it should reactivate');
   };
+
+  const StudentInfo = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+
   return (
     <Grid container spacing={2}>
       <Grid item xs={4}>
@@ -31,6 +43,16 @@ export default function Student() {
           confirmHandler={reactivateHandler}
         />
       </Grid>
+      <a href="./studentInfo.js">Student Info</a>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+          {Array.from(Array(1)).map((_, index) => (
+            <Grid item xs={2} sm={4} md={4} key={props}>
+              <StudentInfo>Student Info </StudentInfo>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </Grid>
   );
 }
