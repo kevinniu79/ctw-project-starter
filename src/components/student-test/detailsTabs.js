65126ed37229a9143a795298222f094dc11c00b9
Noah Ren <nren2025@gmail.com>
2022-07-10 22:44:44 -0500

    Trying tabs
    Probably large fail

--- src/components/student-test/detailsTabs.js
+++ src/components/student-test/detailsTabs.js
@@ -1,19 +1,76 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
-export default function DisabledTabs() {
-  const [value, setValue] = React.useState(2);
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  // eslint-disable-next-line react/require-default-props
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export default function BasicTabs() {
+  const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+  
 
   return (
-    <Tabs value={value} onChange={handleChange} aria-label="disabled tabs example">
-      <Tab label="Active" />
-      <Tab label="Disabled" disabled />
-      <Tab label="Active" />
-    </Tabs>
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
+          <Tab label="Education" {...a11yProps(0)} />
+          <Tab label="Goals" {...a11yProps(1)} />
+          <Tab label="Other" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        Education
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Goals
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Others
+      </TabPanel>
+    </Box>
   );
 }
