3226e0f77a6533ef4e3e5acb583deec8cbb6cf12
Noah Ren <nren2025@gmail.com>
2022-07-11 14:19:26 -0500

    Education Page/Tabs Done
    Finished Card (Testing) (Can't wait for 100 errors)

--- src/components/student-test/details-tabs-box.js
+++ src/components/student-test/details-tabs-box.js
@@ -17,61 +17,12 @@ const Item = styled(Paper)(({ theme }) => ({
 export default function FullWidthGrid() {
   return (
 
-
     <Box sx={{ flexGrow: 1 }}>
         <Grid container justifyContent='flex-end'>
         <Grid item xs={9}>
         <Grid item xs={12}>
     <BasicTabs> test </BasicTabs>
-          <Item>
-            <h3>
-              <b>Post Secondary Plan</b>
-            </h3>
-            <h5>
-              <b>Plans After College: </b>
-            </h5>
-            <h5>
-              <b>I have applied to a college: </b>
-            </h5>
-            <h5>
-              <b>Colleges they’ve applied to/plan to apply to: </b>
-            </h5>
-            <h5>
-              <b>I have begun my work on my college essay:</b>
-            </h5>
-            <h5>
-              <b>I need help writing my college essay:</b>
-            </h5>
-            <h5>
-              <b>First choice of college:</b>
-            </h5>
-            <h3>
-              <b>College Entrance Exam Information: </b>
-            </h3>
-            <h5>
-              <b>PACT Score: 100 Date of PACT: 10/22/2021</b></h5>
-            <h5>
-              <b>PSAT Score: 100 Date of PSAT: 10/22/2021</b>
-            </h5>
-            <h5>
-              <b>ACT Score: 100 Date of ACT: 10/22/2021</b>
-            </h5>
-            <h5>
-              <b>SAT Score: 100 Date of SAT: 10/22/2021 </b>
-            </h5>
-            <h3>
-              <b>Financial Aid:</b>
-            </h3>
-            <h5>
-              <b>I have already completed the financial aid process:</b>
-            </h5>
-            <h5>
-              <b>I need assistance filling out my FAFSA/Financial aid forms:</b>
-            </h5>
-            <h5>
-              <b>Support they need:</b>
-            </h5>
-          </Item>
+        
         </Grid>
         </Grid>
         </Grid>
--- src/components/student-test/detailsTabs.js
+++ src/components/student-test/detailsTabs.js
@@ -6,6 +6,9 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -41,6 +44,13 @@ function a11yProps(index) {
     'aria-controls': `simple-tabpanel-${index}`,
   };
 }
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'left',
+  color: theme.palette.text.secondary,
+}));
 
 export default function BasicTabs() {
   const [value, setValue] = React.useState(0);
@@ -62,7 +72,69 @@ export default function BasicTabs() {
           <Tab label="Other" {...a11yProps(2)} />
         </Tabs>
       </Box>
-      <TabPanel value={value} index={0}></TabPanel>
+      <TabPanel value={value} index={0}>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container justifyContent="flex-end">
+            <Grid item xs={12}>
+              <Grid item xs={12}>
+                <Item>
+                  <h3>
+                    <b>Post Secondary Plan</b>
+                  </h3>
+                  <h5>
+                    <b>Plans After College: </b>
+                  </h5>
+                  <h5>
+                    <b>I have applied to a college: </b>
+                  </h5>
+                  <h5>
+                    <b>Colleges they’ve applied to/plan to apply to: </b>
+                  </h5>
+                  <h5>
+                    <b>I have begun my work on my college essay:</b>
+                  </h5>
+                  <h5>
+                    <b>I need help writing my college essay:</b>
+                  </h5>
+                  <h5>
+                    <b>First choice of college:</b>
+                  </h5>
+                  <h3>
+                    <b>College Entrance Exam Information: </b>
+                  </h3>
+                  <h5>
+                    <b>PACT Score: 100 Date of PACT: 10/22/2021</b>
+                  </h5>
+                  <h5>
+                    <b>PSAT Score: 100 Date of PSAT: 10/22/2021</b>
+                  </h5>
+                  <h5>
+                    <b>ACT Score: 100 Date of ACT: 10/22/2021</b>
+                  </h5>
+                  <h5>
+                    <b>SAT Score: 100 Date of SAT: 10/22/2021 </b>
+                  </h5>
+                  <h3>
+                    <b>Financial Aid:</b>
+                  </h3>
+                  <h5>
+                    <b>I have already completed the financial aid process:</b>
+                  </h5>
+                  <h5>
+                    <b>
+                      I need assistance filling out my FAFSA/Financial aid
+                      forms:
+                    </b>
+                  </h5>
+                  <h5>
+                    <b>Support they need:</b>
+                  </h5>
+                </Item>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Box>
+      </TabPanel>
       <TabPanel value={value} index={1}></TabPanel>
       <TabPanel value={value} index={2}></TabPanel>
     </Box>
