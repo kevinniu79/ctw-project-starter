a3fee7f79a78b81c1c2c88bf115409c5a944a1e0
Noah Ren <nren2025@gmail.com>
2022-07-11 09:25:48 -0500

    minor tabs file fix

--- src/components/student-test/detailsTabsButton.js
+++ src/components/student-test/detailsTabsButton.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
-export default function BasicButtons() {
+export default function EditButton() {
   return (
     <Stack spacing={2} direction="row">
       <Button variant="Edit">Text</Button>
