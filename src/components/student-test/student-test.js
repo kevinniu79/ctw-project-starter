53d4d33f00dacce3b4e31d6ec1ccde0a476bfece
Holly R <hraetz14@gmail.com>
2022-07-12 12:24:15 -0500

    back button

--- src/components/student-test/student-test.js
+++ src/components/student-test/student-test.js
@@ -1,11 +1,21 @@
 /* eslint-disable prettier/prettier */
+import { useNavigate } from 'react-router-dom';
 import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 import StudentTable from './studentTable';
+import ROUTES from '../../constants/routes';
 
 export default function Student() {
+  const navigate = useNavigate();
+
+  const text = '< Back';
+  const onBackClick = () => {
+    navigate(ROUTES.HOME);
+  };
   return (
     <Stack spacing={2} sx={{ mx:50 }}>
       <StudentTable/>
+      <Button onClick={onBackClick}>{text}</Button>
     </Stack>
   );
 }
