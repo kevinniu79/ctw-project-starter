99ea58fa33a54f6f4faf79ec6ce0215b2acdc1e5
Holly R <hraetz14@gmail.com>
2022-07-12 13:02:49 -0500

    remove waste item

--- src/components/student-test/student-test.js
+++ src/components/student-test/student-test.js
@@ -1,13 +1,11 @@
 import Grid from '@mui/material/Grid';
 import StudentModal from './studentModal';
-import BasicTabs from './studentTabs';
+import BasicTabs from './studentTable';
 
 export default function Student() {
   
   return (
     <Grid container spacing={2} justifyContent='center'   justify="center">
-     
-      
       <Grid   item xs={8}>
       <BasicTabs > </BasicTabs>
       </Grid>
--- src/components/student-test/studentTable.js
+++ src/components/student-test/studentTable.js
@@ -11,7 +11,6 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import TablePagination from '@mui/material/TablePagination';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
