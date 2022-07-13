54c1fe87bb2604994c8d123ed1c1e6a090ec35ec
Holly R <hraetz14@gmail.com>
2022-07-13 10:12:20 -0500

    fixed again

--- src/components/student-test/detailsTabs.js
+++ src/components/student-test/detailsTabs.js
@@ -76,73 +76,70 @@ export default function TabsFunction() {
         <Box sx={{ flexGrow: 1 }}>
           <Grid container justifyContent="flex-end">
             <Grid item xs={12}>
-              <Grid item xs={12}>
-                <GridText>
-                  <h3 style={{ color: '#2656A5' }}>
-                    <b>Post Secondary Plan</b>
-                  </h3>
-                  <h5>
-                    <b>Plans After College:</b>
-                  </h5>
-                  <h5>
-                    <b>I have applied to a college: </b>
-                  </h5>
-                  <h5>
-                    <b>Colleges they’ve applied to/plan to apply to: </b>
-                  </h5>
-                  <h5>
-                    <b>I have begun my work on my college essay:</b>
-                  </h5>
-                  <h5>
-                    <b>I need help writing my college essay:</b>
-                  </h5>
-                  <h5>
-                    <b>First choice of college:</b>
-                  </h5>
-                  <h3 style={{ color: '#2656A5' }}>
-                    <b>College Entrance Exam Information: </b>
-                  </h3>
-                  <h5>
-                    <b>
-                      PACT Score: 100
-                      <div>Date of PACT: 10/22/2021</div>
-                    </b>
-                  </h5>
-                  <h5>
-                    <b>
-                      PSAT Score: 100
-                      <div> Date of PSAT: 10/22/2021</div>
-                    </b>
-                  </h5>
-                  <h5>
-                    <b>
-                      ACT Score: 100
-                      <div> Date of ACT: 10/22/2021</div>
-                    </b>
-                  </h5>
-                  <h5>
-                    <b>
-                      SAT Score: 100
-                      <div> Date of SAT: 10/22/2021 </div>
-                    </b>
-                  </h5>
-                  <h3 style={{ color: '#2656A5' }}>
-                    <b>Financial Aid:</b>
-                  </h3>
-                  <h5>
-                    <b>I have already completed the financial aid process:</b>
-                  </h5>
-                  <h5>
-                    <b>
-                      I need assistance filling out my FAFSA/Financial aid
-                      forms:
-                    </b>
-                  </h5>
-                  <h5>
-                    <b>Support they need:</b>
-                  </h5>
-                </GridText>
-              </Grid>
+              <GridText>
+                <h3 style={{ color: '#2656A5' }}>
+                  <b>Post Secondary Plan</b>
+                </h3>
+                <h5>
+                  <b>Plans After College:</b>
+                </h5>
+                <h5>
+                  <b>I have applied to a college: </b>
+                </h5>
+                <h5>
+                  <b>Colleges they’ve applied to/plan to apply to: </b>
+                </h5>
+                <h5>
+                  <b>I have begun my work on my college essay:</b>
+                </h5>
+                <h5>
+                  <b>I need help writing my college essay:</b>
+                </h5>
+                <h5>
+                  <b>First choice of college:</b>
+                </h5>
+                <h3 style={{ color: '#2656A5' }}>
+                  <b>College Entrance Exam Information: </b>
+                </h3>
+                <h5>
+                  <b>
+                    PACT Score: 100
+                    <div>Date of PACT: 10/22/2021</div>
+                  </b>
+                </h5>
+                <h5>
+                  <b>
+                    PSAT Score: 100
+                    <div> Date of PSAT: 10/22/2021</div>
+                  </b>
+                </h5>
+                <h5>
+                  <b>
+                    ACT Score: 100
+                    <div> Date of ACT: 10/22/2021</div>
+                  </b>
+                </h5>
+                <h5>
+                  <b>
+                    SAT Score: 100
+                    <div> Date of SAT: 10/22/2021 </div>
+                  </b>
+                </h5>
+                <h3 style={{ color: '#2656A5' }}>
+                  <b>Financial Aid:</b>
+                </h3>
+                <h5>
+                  <b>I have already completed the financial aid process:</b>
+                </h5>
+                <h5>
+                  <b>
+                    I need assistance filling out my FAFSA/Financial aid forms:
+                  </b>
+                </h5>
+                <h5>
+                  <b>Support they need:</b>
+                </h5>
+              </GridText>
             </Grid>
           </Grid>
         </Box>
@@ -151,73 +148,71 @@ export default function TabsFunction() {
         <Box sx={{ flexGrow: 1 }}>
           <Grid container justifyContent="flex-end">
             <Grid item xs={12}>
-              <Grid item xs={12}>
-                <GridText>
-                  <h3 style={{ color: '#2656A5' }}>
-                    <b>Goal One</b>
-                  </h3>
-                  <h5>
-                    <b>Goal: </b>
-                  </h5>
-                  <h5>
-                    <b>Goal Set Date: </b>
-                  </h5>
-                  <h5>
-                    <b>SEL: </b>
-                  </h5>
-                  <h5>
-                    <b>Goal Review Date: </b>
-                  </h5>
-                  <h5>
-                    <b>Accomplishment State: </b>
-                  </h5>
-                  <h5>
-                    <b>Explanation: </b>
-                  </h5>
-                  <h3 style={{ color: '#2656A5' }}>
-                    <b>Goal Two</b>
-                  </h3>
-                  <h5>
-                    <b>Goal: </b>
-                  </h5>
-                  <h5>
-                    <b>Goal Set Date: </b>
-                  </h5>
-                  <h5>
-                    <b>SEL: </b>
-                  </h5>
-                  <h5>
-                    <b>Goal Review Date: </b>
-                  </h5>
-                  <h5>
-                    <b>Accomplishment State: </b>
-                  </h5>
-                  <h5>
-                    <b>Explanation: </b>
-                  </h5>
-                  <h3 style={{ color: '#2656A5' }}>
-                    <b>Goal Three</b>
-                  </h3>
-                  <h5>
-                    <b>Goal: </b>
-                  </h5>
-                  <h5>
-                    <b>Goal Set Date: </b>
-                  </h5>
-                  <h5>
-                    <b>SEL: </b>
-                  </h5>
-                  <h5>
-                    <b>Goal Review Date: </b>
-                  </h5>
-                  <h5>
-                    <b>Accomplishment State: </b>
-                  </h5>
-                  <h5>
-                    <b>Explanation: </b>
-                  </h5>
-                </GridText>
-              </Grid>
+              <GridText>
+                <h3 style={{ color: '#2656A5' }}>
+                  <b>Goal One</b>
+                </h3>
+                <h5>
+                  <b>Goal: </b>
+                </h5>
+                <h5>
+                  <b>Goal Set Date: </b>
+                </h5>
+                <h5>
+                  <b>SEL: </b>
+                </h5>
+                <h5>
+                  <b>Goal Review Date: </b>
+                </h5>
+                <h5>
+                  <b>Accomplishment State: </b>
+                </h5>
+                <h5>
+                  <b>Explanation: </b>
+                </h5>
+                <h3 style={{ color: '#2656A5' }}>
+                  <b>Goal Two</b>
+                </h3>
+                <h5>
+                  <b>Goal: </b>
+                </h5>
+                <h5>
+                  <b>Goal Set Date: </b>
+                </h5>
+                <h5>
+                  <b>SEL: </b>
+                </h5>
+                <h5>
+                  <b>Goal Review Date: </b>
+                </h5>
+                <h5>
+                  <b>Accomplishment State: </b>
+                </h5>
+                <h5>
+                  <b>Explanation: </b>
+                </h5>
+                <h3 style={{ color: '#2656A5' }}>
+                  <b>Goal Three</b>
+                </h3>
+                <h5>
+                  <b>Goal: </b>
+                </h5>
+                <h5>
+                  <b>Goal Set Date: </b>
+                </h5>
+                <h5>
+                  <b>SEL: </b>
+                </h5>
+                <h5>
+                  <b>Goal Review Date: </b>
+                </h5>
+                <h5>
+                  <b>Accomplishment State: </b>
+                </h5>
+                <h5>
+                  <b>Explanation: </b>
+                </h5>
+              </GridText>
             </Grid>
           </Grid>
         </Box>
@@ -226,73 +221,71 @@ export default function TabsFunction() {
         <Box sx={{ flexGrow: 1 }}>
           <Grid container justifyContent="flex-end">
             <Grid item xs={12}>
-              <Grid item xs={12}>
-                <GridText>
-                  <h3>
-                    <b style={{ color: '#2656A5' }}>Parent Information</b>
-                  </h3>
-                  <h5>
-                    <b>First Name: </b>
-                  </h5>
-                  <h5>
-                    <b>Last Name: </b>
-                  </h5>
-                  <h5>
-                    <b>Address: </b>
-                  </h5>
-                  <h5>
-                    <b>Parent Apartment Number: </b>
-                  </h5>
-                  <h5>
-                    <b>City: </b>
-                  </h5>
-                  <h5>
-                    <b>State: </b>
-                  </h5>
-                  <h5>
-                    <b>Zip Code: </b>
-                  </h5>
-                  <h5>
-                    <b>Home Phone: </b>
-                  </h5>
-                  <h5>
-                    <b>Cell Phone: </b>
-                  </h5>
-                  <h5>
-                    <b>Parent Email: </b>
-                  </h5>
-                  <h3>
-                    <b style={{ color: '#2656A5' }}>Guidance Couselor Info: </b>
-                  </h3>
-                  <h5>
-                    <b>I know my guidance counselor: </b>
-                  </h5>
-                  <h3 style={{ color: '#2656A5' }}>
-                    <b>Admin Details: </b>
-                  </h3>
-                  <h5>
-                    <b>Activity Status: </b>
-                  </h5>
-                  <h5>
-                    <b>Acceptance Status: </b>
-                  </h5>
-                  <h3 style={{ color: '#2656A5' }}>
-                    <b>Signatures </b>
-                  </h3>
-                  <h5>
-                    <b>Student Signature: </b>
-                  </h5>
-                  <h5>
-                    <b>Date Signed: </b>
-                  </h5>
-                  <h5>
-                    <b>Parent Signature: </b>
-                  </h5>
-                  <h5>
-                    <b>Date Signed: </b>
-                  </h5>
-                </GridText>
-              </Grid>
+              <GridText>
+                <h3>
+                  <b style={{ color: '#2656A5' }}>Parent Information</b>
+                </h3>
+                <h5>
+                  <b>First Name: </b>
+                </h5>
+                <h5>
+                  <b>Last Name: </b>
+                </h5>
+                <h5>
+                  <b>Address: </b>
+                </h5>
+                <h5>
+                  <b>Parent Apartment Number: </b>
+                </h5>
+                <h5>
+                  <b>City: </b>
+                </h5>
+                <h5>
+                  <b>State: </b>
+                </h5>
+                <h5>
+                  <b>Zip Code: </b>
+                </h5>
+                <h5>
+                  <b>Home Phone: </b>
+                </h5>
+                <h5>
+                  <b>Cell Phone: </b>
+                </h5>
+                <h5>
+                  <b>Parent Email: </b>
+                </h5>
+                <h3>
+                  <b style={{ color: '#2656A5' }}>Guidance Couselor Info: </b>
+                </h3>
+                <h5>
+                  <b>I know my guidance counselor: </b>
+                </h5>
+                <h3 style={{ color: '#2656A5' }}>
+                  <b>Admin Details: </b>
+                </h3>
+                <h5>
+                  <b>Activity Status: </b>
+                </h5>
+                <h5>
+                  <b>Acceptance Status: </b>
+                </h5>
+                <h3 style={{ color: '#2656A5' }}>
+                  <b>Signatures </b>
+                </h3>
+                <h5>
+                  <b>Student Signature: </b>
+                </h5>
+                <h5>
+                  <b>Date Signed: </b>
+                </h5>
+                <h5>
+                  <b>Parent Signature: </b>
+                </h5>
+                <h5>
+                  <b>Date Signed: </b>
+                </h5>
+              </GridText>
             </Grid>
           </Grid>
         </Box>
